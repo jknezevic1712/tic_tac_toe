17e91dcc1f947b1ecd344e7fea6bb960e312a567
Jakov Knezevic <knezevic.jakov@gmail.com>
2024-01-20 21:29:19 +0100

    Store is now persisted in local storage

--- src/lib/store/store.ts
+++ src/lib/store/store.ts
@@ -1,10 +1,18 @@
 import { create } from "zustand";
 // types
 import type { RootState } from "../types/state";
+import { persist } from "zustand/middleware";
 
-const useStore = create<RootState>(() => ({
-  user: null,
-}));
+const useStore = create(
+  persist<RootState>(
+    () => ({
+      user: null,
+    }),
+    {
+      name: "ttt-storage",
+    },
+  ),
+);
 
 const setUser = (user: RootState["user"]) =>
   useStore.setState((state) => ({ ...state, user }));
