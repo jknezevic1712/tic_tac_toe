1f00927d4c089a3902d3c1b937a4db063f0bfb2b
Jakov Knezevic <knezevic.jakov@gmail.com>
2024-01-22 18:09:12 +0100

    Changes:
    Added winner heading when the game is over and a button that redirects us back to the homepage
    
    Small changes to styles to fix game board borders

--- src/app/game/[slug]/page.tsx
+++ src/app/game/[slug]/page.tsx
@@ -1,11 +1,12 @@
 "use client";
+import { useQuery } from "@tanstack/react-query";
 // components
 import { Button } from "~/components/atoms/button/Button";
 // utils
 import "~/styles/gameBoard.css";
 import { useStore } from "~/lib/store/store";
 import { fetchGame, makeMove } from "~/lib/requests/games";
-import { useQuery } from "@tanstack/react-query";
+import Link from "next/link";
 
 type GameBoardProps = {
   params: {
@@ -15,6 +16,7 @@ type GameBoardProps = {
 function GameBoard({ params: { slug } }: GameBoardProps) {
   const user = useStore((state) => state.user);
   const currentGame = useStore((state) => state.currentGame);
+  let fieldID = 0;
 
   useQuery({
     queryKey: ["fetch-game", slug],
@@ -32,30 +34,45 @@ function GameBoard({ params: { slug } }: GameBoardProps) {
 
   return (
     <main className="mb-16 mt-32 flex w-full max-w-7xl flex-col items-center justify-center gap-6 2xl:mt-52">
-      <h1 className="mb-12 text-4xl font-extrabold sm:text-5xl">
+      <h1 className="mb-12 border-b-4 border-zinc-950 p-1 text-4xl font-extrabold sm:text-5xl">
         Game #{slug}
       </h1>
 
+      {currentGame.winner && (
+        <div className="mb-12 flex flex-col items-center justify-center gap-3">
+          <h2 className="text-3xl font-extrabold sm:text-4xl">
+            {`${currentGame.winner?.username} won!`}
+          </h2>
+          <Link href={"/"}>
+            <Button variant="tertiary">Go Back</Button>
+          </Link>
+        </div>
+      )}
+
       <div className="flex w-3/4 flex-wrap items-start md:w-1/2 xl:w-1/3">
         {currentGame?.board.map((row, rowIdx) =>
-          row.map((field, fieldIdx) => (
-            <Button
-              key={rowIdx.toString() + fieldIdx.toString()}
-              type="button"
-              variant="board"
-              className={`field-${fieldIdx} flex h-24 w-1/3 cursor-pointer items-center justify-center transition-all hover:bg-slate-300`}
-              disabled={
-                field ? true : false || currentGame.status === "finished"
-              }
-              onClick={() =>
-                makeMove(user!.token, +slug, { row: rowIdx, col: fieldIdx })
-              }
-            >
-              <span className="text-4xl font-extrabold">
-                {showCorrectSymbol(field)}
-              </span>
-            </Button>
-          )),
+          row.map((field, fieldIdx) => {
+            fieldID++;
+
+            return (
+              <Button
+                key={fieldID}
+                type="button"
+                variant="board"
+                className={`field-${fieldID} flex h-24 w-1/3 cursor-pointer items-center justify-center transition-all hover:bg-slate-300`}
+                disabled={
+                  field ? true : false || currentGame.status === "finished"
+                }
+                onClick={() =>
+                  makeMove(user!.token, +slug, { row: rowIdx, col: fieldIdx })
+                }
+              >
+                <span className="text-4xl font-extrabold">
+                  {showCorrectSymbol(field)}
+                </span>
+              </Button>
+            );
+          }),
         )}
       </div>
     </main>
